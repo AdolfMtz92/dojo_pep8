18b5f5beaf54055b5ca4583e51604bf25310f6e9
Adolfo Gorjón <60853736+AdolfMtz92@users.noreply.github.com>
2023-07-08 19:35:25 -0600

    Update codebreaker.py

--- codebreaker.py
+++ codebreaker.py
@@ -1,34 +1,50 @@
-trueNumber = "1010";
-
 class Codebreaker:
-    
-  def adivinar(self, numero=None):
-    if trueNumber == '':
-      return 'Number is not defined'
+    def __init__(self, true_number):
+        """
+        Inicializa el objeto Codebreaker con el número verdadero.
+        Args:
+            true_number (str): El número verdadero a adivinar.
+        """
+        self.true_number = true_number
 
-    if numero is None or len(numero) != 4 or 'e' not in list(numero):
-      return "error"
-    
-    if numero == trueNumber:
-      return True
+    def adivinar(self, numero=None):
+        """
+        Adivina el número ingresado y devuelve el resultado.
+        Args:
+            numero (str): El número a adivinar.
+        Returns:
+            str: El resultado de la adivinanza ('X' para dígitos correctos en la posición correcta, '_' para dígitos correctos en la posición incorrecta).
+        """
+        if self.true_number == '':
+            return 'Number is not defined'
 
-    resultadoX  = ''
-    resultado_  = ''
-    arrayNumber = []
-    
-    for x in len(numero):
-      if(arrayNumber[numero[x]] == True):
-        return 'error'
+        if numero is None or len(numero) != 4 or 'e' not in numero:
+            return 'error'
 
-      arrayNumber[numero[x]] = True
-    
-    numero = list(numero)
+        if numero == self.true_number:
+            return True
 
-    for index, x in numero:
-      if trueNumber[index] == numero[index]:
-        resultadoX+='X'
+        resultadoX = ''
+        resultado_ = ''
+        arrayNumber = [False] * 10
 
-      elif x in trueNumber:
-        resultado_='_'
+        for x in range(len(numero)):
+            if arrayNumber[int(numero[x])]:
+                return 'error'
+            arrayNumber[int(numero[x])] = True
 
-    return resultadoX+resultado_
+        for index, x in enumerate(numero):
+            if self.true_number[index] == x:
+                resultadoX += 'X'
+            elif x in self.true_number:
+                resultado_ += '_'
+
+        return resultadoX + resultado_
+
+
+def jugar_codebreaker():
+    true_number = "1010"
+    codebreaker = Codebreaker(true_number)
+
+    intentos_totales = 10
+
